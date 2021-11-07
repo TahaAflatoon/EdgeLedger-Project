1fad4edd18a98b7ff5d35887e814c35d80fa9a40
TahaAflatoon <82364316+TahaAflatoon@users.noreply.github.com>
2021-11-08 03:06:00 +0330

    improved readability

--- edgeledger.js
+++ edgeledger.js
@@ -41,6 +41,7 @@ indicator()
 function Modal() {
     // Get the modal
     const modal = document.querySelector('#whomodal');
+
     // Get the image and insert it inside the modal - use its "alt" text as a caption
     const img = document.querySelector('#whoimage');
     const modalImg = document.querySelector('#img01');
@@ -50,8 +51,10 @@ function Modal() {
         modalImg.src = this.src;
         captionText.innerHTML = this.alt;
     }
+
 // Get the <span> element that closes the modal
 const span = document.querySelectorAll('.close')[0];
+
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = 'none';
